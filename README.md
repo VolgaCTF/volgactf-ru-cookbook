d4ec53fed4767a731d6cabe5f135576f27284c99
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2015-12-29 21:32:03 +0400

    Move cookbook to its own repo

---
+++ README.md
@@ -0,0 +1,5 @@
+# volgactf-ru
+A cookbook to provision server running [volgactf.ru](https://volgactf.ru).
+
+## License
+MIT @ [Alexander Pyatkin](https://github.com/aspyatkin)
---
+++ attributes/default.rb
@@ -0,0 +1,10 @@
+id = 'volgactf-ru'
+
+default[id][:user] = 'vagrant'
+default[id][:group] = 'vagrant'
+default[id][:fqdn] = 'volgactf.dev'
+
+default[id][:repository] = 'https://github.com/VolgaCTF/volgactf.ru'
+default[id][:revision] = 'master'
+
+default[id][:hsts_max_age] = 15768000
