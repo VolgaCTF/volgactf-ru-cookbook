8a17d841114c4e0dda9db26a151d4a587011a2fe
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2016-01-07 20:12:48 +0400

    Support HPKP

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,3 +8,5 @@ default[id][:repository] = 'https://github.com/VolgaCTF/volgactf.ru'
 default[id][:revision] = 'master'
 
 default[id][:hsts_max_age] = 15768000
+default[id][:hpkp_max_age] = 604800
+
--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'volgactf-ru'
 description 'Installs and configures volgactf.ru'
-version '1.1.0'
+version '1.2.0'
 
 recipe 'volgactf-ru', 'Installs and configures volgactf.ru'
 depends 'latest-git', '~> 1.1.2'
