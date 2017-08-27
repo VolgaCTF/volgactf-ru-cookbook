1c83dc71da1a7b1c06bf0021a5f0b96726306372
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2017-08-27 15:53:29 +0400

    Mass updates

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,14 +1,8 @@
 id = 'volgactf-ru'
 
-default[id]['user'] = nil
-
-default[id]['main']['fqdn'] = nil
-default[id]['main']['github_repository'] = 'VolgaCTF/2015.volgactf.ru'
-default[id]['main']['revision'] = '2016-finals'
-
-default[id]['arch_2015']['fqdn'] = nil
-default[id]['arch_2015']['github_repository'] = 'VolgaCTF/2015.volgactf.ru'
-default[id]['arch_2015']['revision'] = 'master'
+default[id]['fqdn'] = nil
+default[id]['github_repository'] = 'VolgaCTF/volgactf-main-site'
+default[id]['revision'] = 'master'
 
 default[id]['hsts_max_age'] = 15_768_000
 default[id]['hpkp_max_age'] = 604_800
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,4 +1,71 @@
-include_recipe 'latest-nodejs::default'
-include_recipe 'modern_nginx::default'
-include_recipe 'volgactf-ru::main'
-include_recipe 'volgactf-ru::arch_2015'
+id = 'volgactf-ru'
+
+h = ::ChefCookbook::Instance::Helper.new(node)
+
+fqdn = node[id]['fqdn']
+base_dir = ::File.join('/var/www', fqdn)
+is_development = node.chef_environment.start_with?('development')
+
+directory base_dir do
+  owner h.instance_user
+  group h.instance_group
+  mode 0755
+  recursive true
+  action :create
+end
+
+repository_url = "https://github.com/#{node[id]['github_repository']}"
+
+git base_dir do
+  repository repository_url
+  revision node[id]['revision']
+  enable_checkout false
+  user h.instance_user
+  group h.instance_group
+  action :sync
+end
+
+nodejs_npm "Install npm packages at #{base_dir}" do
+  package '.'
+  path base_dir
+  json true
+  user h.instance_user
+  group h.instance_group
+end
+
+execute "Build assets at #{base_dir}" do
+  command 'npm run build'
+  cwd base_dir
+  user h.instance_user
+  group h.instance_group
+  environment(
+    'HOME' => h.instance_user_home,
+    'NODE_ENV' => node.chef_environment
+  )
+end
+
+tls_certificate fqdn do
+  action :deploy
+end
+
+tls_item = ::ChefCookbook::TLS.new(node).certificate_entry(fqdn)
+
+nginx_site fqdn do
+  template 'nginx.conf.erb'
+  variables(
+    fqdn: fqdn,
+    ssl_certificate: tls_item.certificate_path,
+    ssl_certificate_key: tls_item.certificate_private_key_path,
+    hsts_max_age: node[id]['hsts_max_age'],
+    access_log: ::File.join(node['nginx']['log_dir'], "#{fqdn}_access.log"),
+    error_log: ::File.join(node['nginx']['log_dir'], "#{fqdn}_error.log"),
+    doc_root: ::File.join(base_dir, 'build'),
+    oscp_stapling: !is_development,
+    scts: !is_development,
+    scts_dir: tls_item.scts_dir,
+    hpkp: !is_development,
+    hpkp_pins: tls_item.hpkp_pins,
+    hpkp_max_age: node[id]['hpkp_max_age']
+  )
+  action :enable
+end
