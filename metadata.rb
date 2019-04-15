773f03e3c35c40fffb964cf67c29c63c0aa3b544
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2019-04-15 11:39:01 +0400

    Update dependencies

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 name 'volgactf-ru'
 description 'Installs and configures volgactf.ru'
-version '1.7.0'
+version '1.8.0'
 
 recipe 'volgactf-ru', 'Installs and configures volgactf.ru'
 depends 'nodejs'
-depends 'nginx'
-depends 'tls', '~> 3.1.0'
+depends 'ngx', '>= 2.1.0'
+depends 'tls', '>= 3.2.0'
 depends 'instance', '~> 2.0.0'
--- recipes/default.rb
+++ recipes/default.rb
@@ -43,44 +43,39 @@ execute "Build assets at #{base_dir}" do
   )
 end
 
+ngx_vhost_variables = {
+  fqdn: fqdn,
+  doc_root: ::File.join(base_dir, 'build'),
+  hsts_max_age: node[id]['hsts_max_age'],
+  oscp_stapling: node[id]['oscp_stapling'],
+  resolvers: %w[1.1.1.1 8.8.8.8 1.0.0.1 8.8.4.4],
+  resolver_valid: 600,
+  resolver_timeout: 10,
+  certificate_entries: []
+}
+
 tls_rsa_certificate fqdn do
   action :deploy
 end
 
 tls_helper = ::ChefCookbook::TLS.new(node)
-tls_rsa_item = tls_helper.rsa_certificate_entry(fqdn)
-tls_ec_item = nil
-ec_certificates = tls_helper.has_ec_certificate?(fqdn)
+ngx_vhost_variables[:certificate_entries] << tls_helper.rsa_certificate_entry(fqdn)
 
-if ec_certificates
+if tls_helper.has_ec_certificate?(fqdn)
   tls_ec_certificate fqdn do
     action :deploy
   end
 
-  tls_ec_item = tls_helper.ec_certificate_entry(fqdn)
+  ngx_vhost_variables[:certificate_entries] << tls_helper.ec_certificate_entry(fqdn)
 end
 
-ngx_vhost_variables = {
-  fqdn: fqdn,
-  ssl_rsa_certificate: tls_rsa_item.certificate_path,
-  ssl_rsa_certificate_key: tls_rsa_item.certificate_private_key_path,
-  hsts_max_age: node[id]['hsts_max_age'],
-  access_log: ::File.join(node['nginx']['log_dir'], "#{fqdn}_access.log"),
-  error_log: ::File.join(node['nginx']['log_dir'], "#{fqdn}_error.log"),
-  doc_root: ::File.join(base_dir, 'build'),
-  oscp_stapling: node[id]['oscp_stapling'],
-  ec_certificates: ec_certificates
-}
-
-if ec_certificates
-  ngx_vhost_variables.merge!({
-    ssl_ec_certificate: tls_ec_item.certificate_path,
-    ssl_ec_certificate_key: tls_ec_item.certificate_private_key_path
-  })
-end
-
-nginx_site fqdn do
+nginx_vhost fqdn do
   template 'nginx.conf.erb'
-  variables ngx_vhost_variables
+  variables(lazy {
+    ngx_vhost_variables.merge(
+      access_log: ::File.join(node.run_state['nginx']['log_dir'], "#{fqdn}_access.log"),
+      error_log: ::File.join(node.run_state['nginx']['log_dir'], "#{fqdn}_error.log")
+    )
+  })
   action :enable
 end
